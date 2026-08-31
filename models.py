e17fdd327156cc63e53578dd4dc0198f2088a6e9
Iceveil <mihanivanovo@mail.ru>
2026-08-31 19:36:01 +0300

    Update models.py

--- models.py
+++ models.py
@@ -7,16 +7,26 @@ from database import Base
 class Recept(Base):
     """Таблица рецептов"""
     __tablename__ = 'recepts'
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     recept_name = Column(String, index=True)
     count = Column(Integer, index=True)
     time_to_done = Column(Integer, index=True)
     children = relationship('ReceptDetails', back_populates='parent')
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "recept_name": self.recept_name,
+            "count": self.count,
+            "time_to_done": self.time_to_done
+        }
+
 
 class ReceptDetails(Base):
     """Таблица с подробным описанием рецептов"""
     __tablename__ = 'recept_details'
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     recept_name = Column(String, index=True)
     time_to_done = Column(Integer, index=True)
@@ -24,3 +34,11 @@ class ReceptDetails(Base):
     description = Column(String, index=True)
     parent_id = Column(Integer, ForeignKey('recepts.id'))
     parent = relationship('Recept', back_populates='children')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "recept_name": self.recept_name,
+            "count": self.count,
+            "time_to_done": self.time_to_done
+        }
