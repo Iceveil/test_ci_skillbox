a06999571c1da7b33a53dd87ad65f1a3747f5406
Iceveil <mihanivanovo@mail.ru>
2026-09-01 20:41:11 +0300

    Update test_fastapi.py

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -1,26 +1,24 @@
 import pytest
 from sqlalchemy import select
-from models import Recept, ReceptDetails
+
+from ..models import Recept, ReceptDetails
 
 
 @pytest.mark.asyncio
 async def test_get_recipes(client, db_session):
-    print('ТЕСТ 1 ЗАПУСТИЛСЯ')
-    rec = Recept(
-        recept_name = 'chicken',
-        time_to_done = 25,
-        count=0
-    )
+    print("ТЕСТ 1 ЗАПУСТИЛСЯ")
+    rec = Recept(recept_name="chicken", time_to_done=25, count=0)
 
     db_session.add(rec)
     await db_session.commit()
 
     from sqlalchemy import select
+
     result = await db_session.execute(select(Recept))
     recipes = result.scalars().all()
     print(f"Рецепты в БД: {[r.recept_name for r in recipes]}")
 
-    resp = await client.get('/recipes')
+    resp = await client.get("/recipes")
 
     assert resp.status_code == 200
     data = resp.read()
@@ -30,51 +28,56 @@ async def test_get_recipes(client, db_session):
 
 @pytest.mark.asyncio
 async def test_get_recept_by_id(client, db_session):
-    print('ТЕСТ 2 ЗАПУСТИЛСЯ!')
+    print("ТЕСТ 2 ЗАПУСТИЛСЯ!")
     rec = ReceptDetails(
-        recept_name='chicken',
-        time_to_done = 25,
-        ing_list = 'chicken solt oil',
-        description = 'very nice',
-        parent_id = 1,
+        recept_name="chicken",
+        time_to_done=25,
+        ing_list="chicken solt oil",
+        description="very nice",
+        parent_id=1,
     )
 
     db_session.add(rec)
     await db_session.commit()
 
     from sqlalchemy import select
+
     result = await db_session.execute(select(ReceptDetails))
     recipes = result.scalars().all()
     print(f"Рецепты в БД: {[r.recept_name for r in recipes]}")
 
-    resp = await client.get('/recipes/1')
+    resp = await client.get("/recipes/1")
     assert resp.status_code == 200
     data = resp.json()
     print(data)
-    assert data.get('Название') == 'chicken'
+    assert data.get("Название") == "chicken"
 
 
 @pytest.mark.asyncio
 async def test_post_recipes(client, db_session):
     rec = {
-        'recept_name' : 'chicken1',
-        'time_to_done': 25,
-        'ing_list' : 'chicken solt oil',
-        'description' : 'very nice',
+        "recept_name": "chicken1",
+        "time_to_done": 25,
+        "ing_list": "chicken solt oil",
+        "description": "very nice",
     }
 
-    resp = await client.post('/recipes', json=rec)
+    resp = await client.post("/recipes", json=rec)
     assert resp.status_code == 200
     data = resp.json()
     print(data)
-    assert data.get('OK') == 'Рецепт chicken1 добавлен'
+    assert data.get("OK") == "Рецепт chicken1 добавлен"
 
-    result = await db_session.execute(select(Recept).where(Recept.recept_name=='chicken1'))
+    result = await db_session.execute(
+        select(Recept).where(Recept.recept_name == "chicken1")
+    )
     res = result.scalar_one_or_none()
-    assert res.recept_name == 'chicken1'
+    assert res.recept_name == "chicken1"
     assert res.time_to_done == 25
 
-    result_2 = await db_session.execute(select(ReceptDetails).where(ReceptDetails.id == 1))
+    result_2 = await db_session.execute(
+        select(ReceptDetails).where(ReceptDetails.id == 1)
+    )
     res_2 = result_2.scalar_one_or_none()
-    assert res_2.recept_name == 'chicken'
+    assert res_2.recept_name == "chicken"
     assert res_2.time_to_done == 25
