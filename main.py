f17419bc7923048e909ede96f706ed34a757bfed
Iceveil <mihanivanovo@mail.ru>
2026-09-01 20:39:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
-from fastapi import FastAPI, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends, FastAPI
 from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import schemas
-from .database import get_db, engine, Base
-from .models import ReceptDetails, Recept
 
+from .database import Base, engine, get_db
+from .models import Recept, ReceptDetails
 
 app = FastAPI()
 
 
-@app.get('/recipes')
-async def get_recipes(db: AsyncSession = Depends(get_db))-> List[Dict[str, Any]]:
+@app.get("/recipes")
+async def get_recipes(db: AsyncSession = Depends(get_db)) -> List[Dict[str, Any]]:
     """Эндпоинт по получению всех рецпетов. Сортируется
     по количеству просмотров и времени приготовления"""
     query = select(Recept).order_by(Recept.count.desc(), Recept.time_to_done.asc())
@@ -22,32 +22,38 @@ async def get_recipes(db: AsyncSession = Depends(get_db))-> List[Dict[str, Any]]
     return [r.to_dict() for r in recipes]
 
 
-@app.get('/recipes/{recipes_id}')
-async def get_recipes_by_id(recipes_id: int, db: AsyncSession = Depends(get_db))-> Dict[str, Any]:
+@app.get("/recipes/{recipes_id}")
+async def get_recipes_by_id(
+    recipes_id: int, db: AsyncSession = Depends(get_db)
+) -> Dict[str, Any]:
     """Эндпойнт по получению одного рецепта по его айди"""
-    query = (select(ReceptDetails.recept_name, ReceptDetails.time_to_done,
-                   ReceptDetails.description, ReceptDetails.ing_list)
-            .where(ReceptDetails.parent_id == recipes_id))
+    query = select(
+        ReceptDetails.recept_name,
+        ReceptDetails.time_to_done,
+        ReceptDetails.description,
+        ReceptDetails.ing_list,
+    ).where(ReceptDetails.parent_id == recipes_id)
     result = await db.execute(query)
     recept = result.all()
 
-    upd_count = (update(Recept).where(Recept.id == recipes_id)
-                 .values(count=Recept.count+1))
+    upd_count = (
+        update(Recept).where(Recept.id == recipes_id).values(count=Recept.count + 1)
+    )
 
     await db.execute(upd_count)
 
     return {
-        'Название': recept[0][0],
-        'Время приготовления': recept[0][1],
-        'Описание': recept[0][2],
-        'Ингредиенты': recept[0][3]
+        "Название": recept[0][0],
+        "Время приготовления": recept[0][1],
+        "Описание": recept[0][2],
+        "Ингредиенты": recept[0][3],
     }
 
 
-
-@app.post('/recipes')
-async def add_recipes(recept: schemas.AddOneRecept,
-                      db: AsyncSession = Depends(get_db))-> Dict[str, str]:
+@app.post("/recipes")
+async def add_recipes(
+    recept: schemas.AddOneRecept, db: AsyncSession = Depends(get_db)
+) -> Dict[str, str]:
     """Добавление рецепта"""
     add_in_recepts = Recept(
         recept_name=recept.recept_name,
@@ -63,17 +69,17 @@ async def add_recipes(recept: schemas.AddOneRecept,
         time_to_done=recept.time_to_done,
         ing_list=recept.ing_list,
         description=recept.description,
-        parent_id=add_in_recepts.id
+        parent_id=add_in_recepts.id,
     )
 
     db.add(new_recept)
     await db.commit()
-    return {'OK': f'Рецепт {recept.recept_name} добавлен'}
+    return {"OK": f"Рецепт {recept.recept_name} добавлен"}
 
 
-@app.post('/create_db')
-async def create_db()-> Dict[str, bool]:
+@app.post("/create_db")
+async def create_db() -> Dict[str, bool]:
     """Создание таблиц. Запустить перед остальными операциями"""
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        return {'ok': True}
+        return {"ok": True}
